04639965852cb22f91f5063625b7c168649ea88a
AkifAshfaq <ranaakifashfaq2005@gmail.com>
2025-12-25 08:16:50 +0000

    data types

---
+++ class-01-basics/02-dataTypes.js
@@ -0,0 +1,9 @@
+let name = "AkifAshfaq"   // type : string 
+let age = 20              // type : number
+let isLoggedIn = true     // type : boolean
+let state;                // type : undefined 
+let random  = null        // type : object 
+let userDetail = {}       // type : object 
+let users = []            // type : object 
+
+console.log(typeof random);
