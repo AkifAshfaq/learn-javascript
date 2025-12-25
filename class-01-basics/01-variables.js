c3565d6857bb2f48ea6687b803596fb351e14b31
AkifAshfaq <ranaakifashfaq2005@gmail.com>
2025-12-25 07:32:50 +0000

    learn variables

---
+++ class-01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345566;
+let accountEmail = "akif@gmail.com";
+var accountPassword = "767776";
+accountCity ="karachi";
+
+// accountId = 4554;   // Not Allowed 
+console.log(accountId);
+
+accountEmail = "abc@gmail.com";
+accountPassword = "09876";
+accountCity = "Rawalpindi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
