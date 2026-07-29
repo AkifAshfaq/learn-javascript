7a9edb5f445d53bd5b4fdac4fbcf3c7b19057c73
Akif Ashfaq <ranaakifashfaq2005@gmail.com>
2026-07-29 07:15:28 +0500

    Added practice code for variables

--- 01-variables/index.js
+++ 01-variables/index.js
@@ -1,1 +1,57 @@
+// variable declaration
+let name;
+// variable initialization
+name = "Akif Ashfaq";
+// print variable value
+console.log(name);
 
+// let age = 21;
+// console.log(age);
+
+// let a;
+// let b;
+// let c;
+
+// a = 10;
+// b = 20;
+// c = a + b;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+// let a ,b , c;
+// a = 10; 
+// b = 20;
+// c = a + b;
+// console.log(a,b,c);
+
+// let a = 10, b = 20, c = a + b;
+
+// let a = 10;
+// let b = 20;
+// let c = a + b;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(a,b,c); 
+
+// {
+//     let age = 21;
+//     console.log(age);
+// }
+
+// console.log(age);
+// this will give an error because the variable age is declared inside a block and it is not accessible outside of that block.
+// console.log("hello") 
+
+
+// const pi = 3.14;
+// console.log(pi); 
+
+// let price1 = 350;
+// let price2 = 450;
+// let totalPrice = price1 + price2;
+// console.log(totalPrice);
